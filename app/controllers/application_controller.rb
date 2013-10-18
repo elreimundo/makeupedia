68841a57c945f6aed30e4a5e5d721899387dadc7
Dev Bootcamp <Apprentice@dbc33.local>
2013-10-18 14:47:35 -0700

    refactors controller by putting logic in a helper method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def build_the_json(params)
+    uri = (params[:url].empty? ? URI.parse("http://en.wikipedia.org/wiki/Internet") : URI.parse(params[:url]))
+    search_text = (params[:search].empty? ? Regexp.new("internet", Regexp::IGNORECASE) : Regexp.new(params[:search], Regexp::IGNORECASE))
+    replace_text = (params[:replace].empty? ? "Al Gore" : params[:replace])
+
+    content = Net::HTTP.get_response(uri).body.force_encoding("UTF-8")
+    { content: content.gsub(search_text, replace_text) }
+  end
 end
--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -3,25 +3,7 @@ require 'net/http'
 class WikisController < ApplicationController
 
   def new
-    uri = URI.parse(params[:url])
-    search_text = Regexp.new(params[:search], Regexp::IGNORECASE)
-    replace_text = params[:replace]
-
-    ## hard-code
-    uri = URI.parse("http://en.wikipedia.org/wiki/Internet")
-    search_text = Regexp.new("the",Regexp::IGNORECASE)
-    replace_text = "and"
-
-
-    content = Net::HTTP.get_response(uri).body
-    content.force_encoding("UTF-8")
-
-    puts content.class
-    puts search_text
-    puts replace_text
-    content_replaced = content.gsub(search_text, replace_text)
-
-    data = { content: content_replaced }
+    data = build_the_json(params)
     render json: data.to_json
   end
 
