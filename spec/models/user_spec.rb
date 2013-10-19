e46b4def7ba6a4ec3915caa1852cf9d2186df975
Dev Bootcamp <Apprentice@dbc33.local>
2013-10-19 15:03:48 -0700

    built form for user on users#new

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,6 @@ describe User do
   it { should validate_presence_of(:email) }
   it { should have_many(:pages)}
 
-  it { should validate_presence_of :password, :on => :create}
+  it { should validate_presence_of(:password), :on => :create}
 
 end
