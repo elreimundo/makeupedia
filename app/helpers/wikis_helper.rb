348abb26dff938dde1a931b03fa4a7cbc1fdef2c
Dev Bootcamp <Apprentice@dbc33.local>
2013-10-18 17:40:30 -0700

    beautiful recursive method to replace only text parts of the page using nokogiri

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,10 +1,21 @@
 module WikisHelper
-    def build_the_json(params)
-    uri = (params[:url].empty? ? URI.parse("http://en.wikipedia.org/wiki/Internet") : URI.parse(params[:url]))
-    search_text = (params[:search].empty? ? Regexp.new("internet", Regexp::IGNORECASE) : Regexp.new(params[:search], Regexp::IGNORECASE))
-    replace_text = (params[:replace].empty? ? "Al Gore" : params[:replace])
+  def build_the_json(params)
+    @data = {uri: (params[:url].empty? ? URI.parse("http://en.wikipedia.org/wiki/Internet") : URI.parse(params[:url])),
+    search_text: (params[:search].empty? ? Regexp.new("internet", Regexp::IGNORECASE) : Regexp.new(params[:search], Regexp::IGNORECASE)),
+    replace_text: (params[:replace].empty? ? "Al Gore" : params[:replace])}
 
-    content = Net::HTTP.get_response(uri).body.force_encoding("UTF-8")
-    { content: content.gsub(search_text, replace_text) }
+    build_the_page
+  end
+
+  def build_the_page
+    content = Net::HTTP.get_response(@data[:uri]).body.force_encoding('UTF-8')
+    object_to_break = Nokogiri::HTML(content)
+    break_the_object(object_to_break)
+    { content: object_to_break.serialize(:encoding => 'UTF-8') }
+  end
+
+  def break_the_object(node)
+      return node.content = node.content.to_s.gsub(@data[:search_text],@data[:replace_text]) if node.children.empty? && node.text?
+      node.children.each{|e| break_the_object(e)} unless node.children.empty?
   end
 end
