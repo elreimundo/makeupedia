ca4a950b36d5991ea23050c0f7e13d9b977c1bc0
Dev Bootcamp <Apprentice@dbc33.local>
2013-10-19 14:54:25 -0700

    rails generate sessions and move sessions create -> users create

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+
+    end
+  end
+end
