109ada5d7ff8a24eebf37dfee4be80fd15768e20
Dev Bootcamp <Apprentice@dbc33.local>
2013-10-19 15:09:04 -0700

    users#create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(email: params[:email], password: params[:password])
+    if @user.save
+      puts "Yes! Victory!"
+    else
+      puts "Oh, noes!!!"
+    end
+  end
 end
