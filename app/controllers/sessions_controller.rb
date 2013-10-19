fff6d817af17f99d55589f9b776eea609cea602e
Dev Bootcamp <Apprentice@dbc33.local>
2013-10-19 15:34:46 -0700

    working on login route, probably not complete yet

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,11 @@
 class SessionsController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def create
+    puts params
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
