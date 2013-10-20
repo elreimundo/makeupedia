4990324d118e76e54c2875c837d6959e9175910e
David Reiman <david.reiman@gmail.com>
2013-10-19 21:14:15 -0700

    changes to pages are now saved and associated properly when user is signed in and requests a new page

--- app/models/change.rb
+++ app/models/change.rb
@@ -1,7 +1,6 @@
 class Change < ActiveRecord::Base
-  attr_accessible :find_text, :replace_text
+  attr_accessible :find_text, :replace_text, :page_user_id
   belongs_to :page_user
   validates :find_text, presence: true
   validates :replace_text, presence: true
-
 end
