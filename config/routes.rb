9c16dd60158122ba7998896f7c727d31e775d591
Elmer <sky@fox.local>
2013-10-21 12:27:03 -0700

    fix typo in route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Mwiki::Application.routes.draw do
   match "/login", to: 'sessions#new', as: '/login'
   match "/logout", to: 'sessions#destroy', as: '/logout'
 
-  get "/wiki/:page", to: 'wiki#revise'
+  get "/wiki/:page", to: 'wikis#revise'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
