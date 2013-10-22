b02190717f3315b38e74293371aef220b3c1b5a5
Dev Bootcamp <Apprentice@dbc33.local>
2013-10-22 10:38:12 -0700

    adjusted routes to ensure deletion can occur

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Mwiki::Application.routes.draw do
   root :to => "wikis#index"
   resources :wikis
   resources :users
-  resources :pages, only: [:destroy]
+  resources :pageusers, only: [:destroy]
   resources :sessions, only: [:new, :create, :destroy]
 
   match "/signup", to: 'users#new', as: '/signup'
@@ -12,7 +12,7 @@ Mwiki::Application.routes.draw do
   get "/wiki/reconstruct/:page", to: 'wikis#reconstruct'
   get "/wiki/:page", to: 'wikis#revise'
 
-  get "/pages/:id/delete"
+  get "/pageusers/:id/delete"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
