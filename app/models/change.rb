03b106ea0baa1fc349e15c6aee7c13770134db84
Dev Bootcamp <Apprentice@dbc33.local>
2013-10-23 12:27:28 -0700

    bug fix - attr_accessible ending for change model

--- app/models/change.rb
+++ app/models/change.rb
@@ -1,5 +1,5 @@
 class Change < ActiveRecord::Base
-  attr_accessible :find_text, :replace_text, :page_user_id
+  attr_accessible :find_text, :replace_text, :page_user_id, :ending
   belongs_to :page_user
   has_many :pages, through: :page_user
   validates :find_text, presence: true
